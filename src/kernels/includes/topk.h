1bcf95c687bc81cc4ddb20882bda335aeeb45375
Chong Chen <chongchen1999@gmail.com>
2024-07-17 12:23:52 -0400

    add topk kernel

---
+++ src/kernels/includes/topk.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <float.h>
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include "src/utils/tensor.h"
+
+template<typename T, int K>
+struct topK {
+    T val[K];
+    int id[K];
+
+    __device__ void init() {
+        for (int i = 0; i < K; ++i) {
+            id[i] = -1;
+            val[i] = -1e-20;
+        }
+    }
+
+    __device__ void insertHeap(T data, int data_id) {
+        float v = static_cast<float>(val[K - 1]);
+        if (id[K - 1] == -1 || v < static_cast<float>(data)) {
+            id[K - 1] = data_id;
+            val[K - 1] = data;
+        }
+        
+        for (int i = K - 2; i >= 0; --i) {
+            if (val[i + 1] > val[i] || id[i] == -1) {
+                T tmp = val[i];
+                val[i] = val[i + 1];
+                val[i + 1] = tmp;
+
+                int tmp_id = id[i];
+                id[i] = id[i + 1];
+                id[i + 1] = tmp_id;
+            }
+        }
+    }
+};
+
+template <typename T>
+void launchTopKforBeamSearch(TensorWrapper<T> *probs,
+                             TensorWrapper<int> *topk_ids,
+                             TensorWrapper<T> *topk_vals,
+                             TensorWrapper<int> *final_topk_ids,
+                             TensorWrapper<T> *final_topk_vals);
