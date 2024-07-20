512d2b43541d94a24bc5b7cd71c5ef46629f7856
Chong Chen <chongchen1999@gmail.com>
2024-07-20 16:53:52 -0400

    update cal_padding_offset kernel

--- src/kernels/includes/cal_padding_offset.h
+++ src/kernels/includes/cal_padding_offset.h
@@ -5,6 +5,14 @@
 #include "src/utils/macro.h"
 #include "src/utils/tensor.h"
 
+/*
+    for example:
+    input_lengths: [4, 3, 5]
+    cum_seqlens: [0, 4, 7, 12]
+    padding_offset: [0, 0, 0, 0,
+                     1, 1, 1,
+                     3, 3, 3, 3, 3]
+*/
 void launchCalPaddingOffset(TensorWrapper<int> *padding_offset, 
                             TensorWrapper<int> *cum_seqlens,
                             TensorWrapper<int> *input_lengths);
