2b0baeceaacdc4991bfc39cb5141be22c9120bc7
Chong Chen <chongchen1999@gmail.com>
2024-07-28 21:36:49 -0400

    update fils

---
+++ lab/cmake/build_static_lib/src/div.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../includes/head.h"
+
+double divide(int a, int b) {
+    return (double)a / b;
+}
---
+++ lab/cmake/build_static_lib/src/sub.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../includes/head.h"
+
+// 你好
+int subtract(int a, int b) {
+    return a - b;
+}
